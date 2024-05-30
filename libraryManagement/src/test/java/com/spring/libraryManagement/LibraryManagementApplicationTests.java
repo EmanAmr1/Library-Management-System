a2e162cfefcc87b52f653162b1dc411429c95285
EmanAmr1 <emanamr543@gmail.com>
2024-05-30 14:46:34 +0300

    initalize project

---
+++ libraryManagement/src/test/java/com/spring/libraryManagement/LibraryManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.libraryManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
